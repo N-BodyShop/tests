eea622ca401aff2b0e41985d76ce5d0cbda14d85
trq <trq>
2006-07-13 23:55:33 +0000

    Fixed up comparisons.

--- density/testpkd.sh
+++ density/testpkd.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+rm -f pkdden.den
 # gasoline density.param
 charmrun +p 16 gasoline density.param
 #
 # difference with the standard
 # 
-echo EXPECT about 0.0002 on the second line
-arrdiff pkdden.den bench.den | arrmax
+echo EXPECT about 7e-7
+../array/subarr pkdden.den bench.den > diff.den
+../array/divarr diff.den bench.den | ../array/maxarr
