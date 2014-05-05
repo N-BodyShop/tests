05d18ff665e9f65de0f7c911b83a74b1b030aa76
Tom Quinn <trq@astro.washington.edu>
2014-05-04 17:51:56 -0700

    Update periodic script.

--- forces/periodic/tstforce_ChaNGa.sh
+++ forces/periodic/tstforce_ChaNGa.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 # get rid of old output
-rm ../lambs.00200_subsamp_30K.acc2
+rm lambs_30K.000000.acc2
 echo Testing forces in ChaNGa
 echo For HEXADECAPOLE:
 echo Expect RMS force errors of .004
 echo Expect Max force errors of .07
 # run program
-../../charmrun +p 2 ../../ChaNGa +f -v 1 -D 1 -c 1 -p 4 -z lambs_30K.param >& DIAG
+../../charmrun +p 2 ++local ../../ChaNGa -v 1 lambs_30K.param >& DIAG
 
-../../array/sortvec ../lambs.00200_subsamp_30K.acc2 ../lambs.00200_subsamp_30K.iord > sorted.acc
-../../array/subarr sorted.acc direct.acc > diff.acc
+../../array/subarr lambs_30K.000000.acc2  direct.acc > diff.acc
 ../../array/magvec < diff.acc > magdiff.arr
 ../../array/magvec < direct.acc > mag.acc
 ../../array/divarr magdiff.arr mag.acc > rdiff.acc
